cd327d533da5a12c4f231b99e5e545556b3908c6
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-16 01:36:12 -0700

    Initialize VitePress
    - Add theme submodule
    - Add initial configuration files
    - Override theme-default VPFooter with custom theme version

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,50 @@
+/*
+  Copyright (C) 2024 Nintendo Homebrew
+  SPDX-License-Identifier: MIT
+*/
+
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitepress'
+
+import container from 'markdown-it-container'
+
+export default defineConfig({
+  title: "Wii U Hacks Guide",
+  description: "A guide to hacking the Nintendo Wii U.",
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^.*\/VPFooter\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/VPFooter.vue', import.meta.url)
+          )
+        }
+      ]
+    }
+  },
+  markdown: {
+    config: (md) => {
+      md.use(container, "tabs", {
+        render: (tokens, idx) => {
+          const token = tokens[idx];
+          if (token.nesting === 1) {
+            return `<Tabs ${token.info}>\n`;
+          } else {
+            return `</Tabs>\n`;
+          }
+        }
+      });
+      md.use(container, 'tab', {
+        render: (tokens, idx) => {
+          const token = tokens[idx];
+          if (token.nesting === 1) {
+            return `<Tab name="${token.info.match(/^ ?tab\s+(.*)$/)[1]}">`;
+          } else {
+            return `</Tab>\n`;
+          }
+        }
+      });
+    }
+  }
+})
