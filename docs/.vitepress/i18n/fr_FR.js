111e4a04b7673790056dbe7c2ebd1c13471b0c14
Lazr1026 <lazarm1026@gmail.com>
2026-06-05 16:46:02 -0700

    Fix French links

--- docs/.vitepress/i18n/fr_FR.js
+++ docs/.vitepress/i18n/fr_FR.js
@@ -5,31 +5,29 @@ const locale = "fr_FR";
 const sidebar_troubleshooting = {
 	text: localeData.troubleshooting,
 	items: [
-		{ text: localeData.pages["common-issues-fixes"], link: `/common-issues-fixes` },
-		{ text: localeData.pages["recover-vwii-ioses-channels"], link: `/recover-vwii-ioses-channels` }
+		{ text: localeData.pages["common-issues-fixes"], link: `/${locale}/common-issues-fixes` },
+		{ text: localeData.pages["recover-vwii-ioses-channels"], link: `/${locale}/recover-vwii-ioses-channels` }
 	]
 }
 
 const sidebar_extras = {
 	text: localeData.extras,
 	items: [
-		{ text: localeData.pages["block-updates"], link: `/block-updates` },
-		{ text: localeData.pages["unblock-updates"], link: `/unblock-updates` },
-		{ text: localeData.pages["dump-games"], link: `/dump-games` },
-		{ text: localeData.pages["uninstall-cbhc"], link: `/uninstall-cbhc` },
-		{ text: localeData.pages["uninstall-indexiine"], link: `/uninstall-indexiine` },
-		{ text: localeData.pages["uninstall-payloadloader"], link: `/uninstall-payloadloader` }
+		{ text: localeData.pages["block-updates"], link: `/${locale}/block-updates` },
+		{ text: localeData.pages["unblock-updates"], link: `/${locale}/unblock-updates` },
+		{ text: localeData.pages["dump-games"], link: `/${locale}/dump-games` },
+		{ text: localeData.pages["uninstall-cbhc"], link: `/${locale}/uninstall-cbhc` },
+		{ text: localeData.pages["uninstall-indexiine"], link: `/${locale}/uninstall-indexiine` },
+		{ text: localeData.pages["uninstall-payloadloader"], link: `/${locale}/uninstall-payloadloader` }
 	]
 }
 
 const sidebar_common = {
 	text: localeData.other,
 	items: [
-		{ text: localeData.pages["about"], link: `/about` },
-		{ text: localeData.pages["donations"], link: `/donations` },
-		{ text: localeData.pages["hashes"], link: `/hashes` },
-		{ text: localeData.pages["privacy-policy"], link: `/privacy-policy` }
-		
+		{ text: localeData.pages["about"], link: `/${locale}/about` },
+		{ text: localeData.pages["donations"], link: `/${locale}/donations` },
+		{ text: localeData.pages["privacy-policy"], link: `/${locale}/privacy-policy` }
 	]
 }
 
@@ -42,26 +40,26 @@ const themeConfig = {
 	returnToTopLabel: localeData.returnToTopLabel,
 
 	nav: [
-		{ text: localeData.pages["faq"], link: `/faq` },
+		{ text: localeData.pages["faq"], link: `/${locale}/faq` },
 		sidebar_troubleshooting,
 		sidebar_extras
 	],
 	sidebar: {
 		/*
-		 	The `/` path needs to be at the bottom as a catch all! If it is placed anywhere above,
+		 	The `/${locale}/` path needs to be at the bottom as a catch all! If it is placed anywhere above,
 			it will select the first matching one and not parse the rest!
 		*/
-		[`/`]: [
+		[`/${locale}/`]: [
 			{
 				text: localeData.guide,
 				items: [
-					{ text: localeData.pages["aroma/getting-started"], link: `/aroma/getting-started` },
-					{ text: localeData.pages["aroma/sd-preparation"], link: `/aroma/sd-preparation` },
-					{ text: localeData.pages["aroma/browser-exploit"], link: `/aroma/browser-exploit` },
-					{ text: localeData.pages["aroma/nand-backup"], link: `/aroma/nand-backup` },
-					{ text: localeData.pages["aroma/installing-payloadloader"], link: `/aroma/installing-payloadloader` },
-					{ text: localeData.pages["aroma/autobooting"], link: `/aroma/autobooting` },
-					{ text: localeData.pages["finalizing-setup"], link: `/aroma/finalizing-setup` }
+					{ text: localeData.pages["aroma/getting-started"], link: `/${locale}/aroma/getting-started` },
+					{ text: localeData.pages["aroma/sd-preparation"], link: `/${locale}/aroma/sd-preparation` },
+					{ text: localeData.pages["aroma/browser-exploit"], link: `/${locale}/aroma/browser-exploit` },
+					{ text: localeData.pages["aroma/nand-backup"], link: `/${locale}/aroma/nand-backup` },
+					{ text: localeData.pages["aroma/installing-payloadloader"], link: `/${locale}/aroma/installing-payloadloader` },
+					{ text: localeData.pages["aroma/autobooting"], link: `/${locale}/aroma/autobooting` },
+					{ text: localeData.pages["finalizing-setup"], link: `/${locale}/aroma/finalizing-setup` }
 				]
 			},
 			sidebar_troubleshooting,
@@ -70,11 +68,11 @@ const themeConfig = {
 		],
 	},
 	footer: {
-		copyright: 'Copyright © 2026 Nintendo Homebrew',
+		copyright: 'Copyright © 2024 Nintendo Homebrew',
 		items: [
-			{ text: localeData.pages["about"], link: `/about` },
-			{ text: localeData.pages["donations"], link: `/donations` },
-			{ text: localeData.pages["privacy-policy"], link: `/privacy-policy` }
+			{ text: localeData.pages["about"], link: `/${locale}/about` },
+			{ text: localeData.pages["donations"], link: `/${locale}/donations` },
+			{ text: localeData.pages["privacy-policy"], link: `/${locale}/privacy-policy` }
 		]
 	}
 };
